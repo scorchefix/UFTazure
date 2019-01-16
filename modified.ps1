08c07c6aefba4fe992be11d641d34468d8f3f671
scorchefix <scorchefixx@abv.bg>
2019-01-16 13:45:16 +0200

    1
    1

---
+++ modified.ps1
@@ -0,0 +1,49 @@
+function Expand-ZIPFile($file, $destination)
+{
+  $shell = new-object -com shell.application
+  $zip = $shell.NameSpace($file)
+  $dest = $shell.NameSpace($destination)
+  foreach($item in $zip.items())
+  {
+
+  $dest.copyhere($item)
+
+  }
+}
+
+function AbsPath($folder)
+{
+   [System.IO.Directory]::SetCurrentDirectory(((Get-Location -PSProvider FileSystem).ProviderPath))
+   $path = [IO.Path]::GetFullPath($folder)
+
+   return $path
+}
+
+
+$Registry_Key = "HKLM:\SOFTWARE\WOW6432Node\Mercury Interactive\QuickTest Professional\CurrentVersion"
+$result = Test-Path $Registry_Key
+
+if($result)
+
+{
+
+   $value = "QuickTest Professional"
+   $uftPath = Get-ItemProperty -Path $Registry_Key | Select-Object -ExpandProperty $value
+
+   $currdir = AbsPath -folder .\
+   $zipFile = Join-Path -Path $currdir -ChildPath "UFT.zip"
+   Expand-ZIPFile $zipFile $currdir
+
+   $launcherPath = Join-Path $currdir -ChildPath "UFTWorking"
+
+   [Environment]::SetEnvironmentVariable("UFT_LAUNCHER", $launcherPath, "Machine")
+}
+
+else
+{
+
+Write-Host "No Unified Functional Testing installation detected"
+
+}
+
+# Current script is working for UFT versions up to 14.03
